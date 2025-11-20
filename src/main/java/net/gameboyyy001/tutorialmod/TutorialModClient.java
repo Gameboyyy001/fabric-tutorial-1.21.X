92a9846d0ea4841e77327146a507d63208c500b0
Gameboyyy001 <GB001content@gmail.com>
2025-11-20 09:49:21 +0100

    Initial commit

---
+++ src/main/java/net/gameboyyy001/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.gameboyyy001.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
